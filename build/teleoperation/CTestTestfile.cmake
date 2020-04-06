222c88940e2d2f1af1e726cd8409554b3e7e49c8
dalabayev <dalabayev.e@gmail.com>
2020-04-06 18:41:59 +0200

    first commit

---
+++ build/teleoperation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yerbolat/catkin_ws/src/teleoperation
+# Build directory: /home/yerbolat/catkin_ws/build/teleoperation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
